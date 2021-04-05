4d3dbed2afa58056192bc47cf940433a33da456e
Christian Kischy Haege <kischy@gmx.de>
2021-04-05 14:21:47 +0200

    Implemented SimplePrime

--- p27-euler/SimplePrime.cs
+++ p27-euler/SimplePrime.cs
@@ -2,13 +2,23 @@
 using System.Collections.Generic;
 using System.Text;
 
+
 namespace p27_euler
 {
     public class SimplePrime
     {
         public bool isPrime(ulong number)
         {
-            return false;
+            if (number < 2) return false;
+            if (number == 2 || number == 3) return true;
+            if (number % 2 == 0) return false;
+
+            for(ulong i = 3; i <= Convert.ToUInt64(Math.Sqrt(number)); i+=2)
+            {
+                if (number % i == 0) return false;
+            }
+
+            return true;
         }
     }
 }

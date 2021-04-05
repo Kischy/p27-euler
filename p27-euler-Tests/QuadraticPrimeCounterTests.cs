4c5ac61ebbd129fafb4b21b762b34cad61caf715
Christian Kischy Haege <kischy@gmx.de>
2021-04-05 14:41:05 +0200

    Addded tests for QuadraticPrimeCounter - added all files

---
+++ p27-euler-Tests/QuadraticPrimeCounterTests.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using p27_euler;
+
+namespace p27_euler_Tests
+{
+    [TestClass]
+    public class QuadraticPrimeCounterTests
+    {
+        QuadraticPrimeCounter qpc = new QuadraticPrimeCounter();
+
+        [TestMethod]
+        public void a_1_b_41_gives_40_primes()
+        {
+            Assert.AreEqual(40, qpc.countConsecutivePrimes(1, 41));
+        }
+
+        [TestMethod]
+        public void a_m79_b_1601_gives_80_primes()
+        {
+            Assert.AreEqual(80, qpc.countConsecutivePrimes(-79, 1601));
+        }
+
+
+    }
+}
